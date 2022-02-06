a179b533aac1f06877feaf0a9f4d3788f1020487
MH01-Hasan <mehediph2021@gmail.com>
2022-02-06 23:08:37 +0600

    setup component

---
+++ src/components/Home/AboutSigma/AboutSigma.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AboutSigma.css'
+
+const AboutSigma = () => {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    );
+};
+
+export default AboutSigma;
