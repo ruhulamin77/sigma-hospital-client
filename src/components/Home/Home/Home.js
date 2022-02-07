dffb40833e98ba69705b24e0abb4269d379a8045
developerfarid <pustfarid333@gmail.com>
2022-02-07 20:06:53 +0600

    footer-done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutSigma from "../AboutSigma/AboutSigma";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <AboutSigma />
+      <Footer />
     </div>
   );
 };
