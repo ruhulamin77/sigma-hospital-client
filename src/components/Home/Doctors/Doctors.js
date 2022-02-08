1252158de1d78b5a835a35409d6aa4f95c75ef7a
Nahid51 <hasannahidnahid@gmail.com>
2022-02-08 19:08:31 +0600

    make doctor serction responsive

--- src/components/Home/Doctors/Doctors.js
+++ src/components/Home/Doctors/Doctors.js
@@ -6,7 +6,6 @@ const Doctors = () => {
     return (
         <div>
             <div className='doctor-section text-center'>
-                <hr />
                 <h5 style={{ letterSpacing: "3px" }}>Doctors</h5>
                 <h1>Our outstanding doctors <br /> <span className='fw-light'> are active to help you!</span></h1>
                 <DoctorsSlider />
