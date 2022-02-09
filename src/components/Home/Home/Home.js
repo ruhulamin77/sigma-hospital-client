0560da42af96edb2935458479f61583dae99c451
MH01-Hasan <mehediph2021@gmail.com>
2022-02-09 21:54:03 +0600

    why section done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AboutSigma from "../AboutSigma/AboutSigma";
+import ChooseUs from "../ChooseUs/ChooseUs";
 import Doctors from "../Doctors/Doctors";
 import Services from "../Services/Services";
 
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <div>
       <AboutSigma />
+      <ChooseUs/>
       <Services />
       <Doctors />
     </div>
