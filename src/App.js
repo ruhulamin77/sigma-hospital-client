2bfb95322153207143194fbf335055f2826d781d
Lukman Hossain <lukman.webdev@gmail.com>
2022-02-10 19:55:33 +0600

    form commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {
   BrowserRouter as Router, Route, Routes
 } from "react-router-dom";
 import Home from "./components/Home/Home/Home";
+import Login from "./components/Login/Login/Login";
+import Register from "./components/Login/Register/Register";
 
 function App() {
   return (
@@ -9,6 +11,12 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Routes>
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
     </div>
