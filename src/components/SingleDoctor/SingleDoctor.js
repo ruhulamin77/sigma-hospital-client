badddae1932c87e57d76c357f1b3c7fd98f371e5
Nahid51 <hasannahidnahid@gmail.com>
2022-02-15 17:36:22 +0600

    added single doctors' information

--- src/components/SingleDoctor/SingleDoctor.js
+++ src/components/SingleDoctor/SingleDoctor.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Table } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
-import watch from '../../image/watch.png';
+import logo from '../../images/logo/logo1.png';
 import './SingleDoctor.css';
 
 const SingleDoctor = () => {
@@ -43,7 +43,7 @@ const SingleDoctor = () => {
                                         <br />
                                         <a href="/" target="_blank" rel="noreferrer"><i className="fab fa-google"></i></a>
                                     </div>
-                                    <div className='mt-auto about-doctor'>
+                                    <div className='mt-auto about-doctor2'>
                                         <h2>{singleDoctor?.name}</h2>
                                         <h5>{singleDoctor?.title}</h5>
                                     </div>
@@ -59,20 +59,35 @@ const SingleDoctor = () => {
                                         <tbody>
 
                                             <tr>
-                                                <td>Mondy-Friday</td>
-                                                <td>6:00 – 7:00 pm</td>
+                                                <td>{singleDoctor?.day?.day1}</td>
+                                                <td>{singleDoctor?.time?.morning}</td>
                                             </tr>
                                             <tr>
-                                                <td>Saturday</td>
-                                                <td>9:00 – 8:00 pm</td>
+                                                <td>{singleDoctor?.day?.day2}</td>
+                                                <td>{singleDoctor?.time?.afternoon}</td>
                                             </tr>
                                             <tr>
-                                                <td>Sunday</td>
-                                                <td>10:00 – 9:00 pm</td>
+                                                <td>{singleDoctor?.day?.day3}</td>
+                                                <td>{singleDoctor?.time?.night}</td>
                                             </tr>
                                         </tbody>
                                     </Table>
                                 </div>
+                            </div>
+                            <div className='contact2'>
+                                <img src={logo} alt="Logo" />
+                                <h2>Sigmacare Services</h2>
+                                <div className='info'>
+                                    <p><b>Call</b> : +123456789</p>
+                                    <p><b>Mail</b> : support@example.com</p>
+                                    <p><b>Address</b> : 1234 North Avenue Luke Lane, South Bend, IN 360001</p>
+                                </div>
+                                <nav>
+                                    <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><i className="fab fa-facebook fa-lg"></i></a>
+                                    <a href="https://www.twitter.com/" target="_blank" rel="noreferrer"><i className="fab fa-twitter fa-lg"></i></a>
+                                    <a href="https://www.youtube.com/" target="_blank" rel="noreferrer"><i className="fab fa-youtube fa-lg"></i></a>
+                                    <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin fa-lg"></i></a>
+                                </nav>
                             </div>
                         </div>
                         <div className='col-md-8 col-12'>
