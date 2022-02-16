1c90c7f0b84791c01769048e29a69a573765a91b
Lukman Hossain <lukman.webdev@gmail.com>
2022-02-17 01:44:40 +0600

    update

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -22,9 +22,9 @@ const Services = () => {
                                 <h6 style={{ letterSpacing: "3px" }}>SERVICES</h6>
                                 <h1>We Provide <br /> <span className='fw-light'> Medical Services</span></h1>
                                 <p className='text-secondary lh-2 mt-3'>It is a long established fact that a reader will be distracted by the <br /> readable content of a page when looking at its layout.</p>
-                                <NavLink to="">
-                                    <button className="service-btn mt-5 mb-3">
-                                        <span>Services</span><i className="fas fa-plus btn-icon"></i>
+                                <NavLink to="/service">
+                                    <button className="services-btn mt-5 mb-3">
+                                       <span>Services</span><i className="fas fa-plus btn-icon"></i>
                                     </button>
                                 </NavLink>
                             </Card.Body>
