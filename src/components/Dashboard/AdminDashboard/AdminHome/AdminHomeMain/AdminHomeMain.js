3036e064cc1e14af14714731ec33f7fe6f14463a
Lukman Hossain <lukman.webdev@gmail.com>
2022-02-17 21:45:33 +0600

    set route

---
+++ src/components/Dashboard/AdminDashboard/AdminHome/AdminHomeMain/AdminHomeMain.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FirstSection from '../FirstSection/FirstSection';
+import FourthSection from '../FourthSection/FourthSection';
+import SecondSection from '../SecondSection/SecondSection';
+import ThirdSection from '../ThirdSection/ThirdSection';
+
+const AdminHomeMain = () => {
+    return (
+        <div>
+            <FirstSection></FirstSection>
+            <SecondSection></SecondSection>
+            <ThirdSection></ThirdSection>
+            <FourthSection></FourthSection>
+        </div>
+    );
+};
+
+export default AdminHomeMain;
