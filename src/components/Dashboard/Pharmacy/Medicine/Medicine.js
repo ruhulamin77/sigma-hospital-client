9e8d8ccccd44ae96a49cf0b0b4fa0871261b3a3e
MH01-Hasan <mehediph2021@gmail.com>
2022-02-23 00:48:48 +0600

    prvlm-solv

--- src/components/Dashboard/Pharmacy/Medicine/Medicine.js
+++ src/components/Dashboard/Pharmacy/Medicine/Medicine.js
@@ -4,31 +4,21 @@ import { useForm } from "react-hook-form";
 import './Medicine.css'
 const Medicine = () => {
     const [medicines, setMedicines] = useState([])
-    const [quantity, setQuantity] = useState('')
     console.log(medicines)
-
-
-
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-
-    const onSubmit = data => {
-        console.log(data)
-    };
-
     useEffect(() => {
         fetch("https://shrouded-headland-44423.herokuapp.com/medicine")
             .then(res => res.json())
             .then(data => setMedicines(data))
     }, [])
 
-    const handelquantity = (e) => {
-        const Quantity = e.target.value;
-        setQuantity(Quantity)
-    }
+
+
+
+
     return (
         <div className='container mt-5 mb-5'>
             <div>
-                {medicines?.map((medicine, index) => (
+                {/* {medicines?.map((medicine, index) => (
                     <form >
                         <input type="text" value={medicine?.brand} readOnly />
                         <input type="text" value={medicine?.name} readOnly />
@@ -42,7 +32,7 @@ const Medicine = () => {
 
 
 
-                ))}
+                ))} */}
 
                 <Table striped bordered hover size="sm" className=' mt-5'>
                     <thead>
@@ -60,18 +50,11 @@ const Medicine = () => {
                     </thead>
                     <tbody>
                         {medicines?.map((medicine, index) => (
-                            <tr>
-                                <td>{index + 1}</td>
-                                <td>{medicine?.brand}</td>
-                                <td>{medicine?.name}</td>
-                                <td>{medicine?.pawer[0]}</td>
-                                <td>{medicine?.type[0]}</td>
-                                <td>{medicine?.stock}</td>
-                                <td>{medicine?.salePrice}</td>
-                                <input type="text" onChange={handelquantity} id={medicine._id} />
-                                <td>{medicine?.salePrice * quantity}</td>
+                            <TableRow medicine={medicine}
+                                index={index}
 
-                            </tr>
+                            ></TableRow>
+
                         ))}
 
                     </tbody>
@@ -83,3 +66,27 @@ const Medicine = () => {
 };
 
 export default Medicine;
+
+const TableRow = ({ medicine, index }) => {
+
+    const [quantity, setQuantity] = useState('')
+
+    const handelquantity = (e) => {
+        console.log(e)
+        const Quantity = e.target.value;
+        setQuantity(Quantity)
+    }
+    return <tr>
+        <td>{index + 1}</td>
+        <td>{medicine?.brand}</td>
+        <td>{medicine?.name}</td>
+        <td>{medicine?.pawer[0]}</td>
+        <td>{medicine?.type[0]}</td>
+        <td>{medicine?.stock}</td>
+        <td>{medicine?.salePrice}</td>
+        <td><input type="text" onChange={handelquantity} id={medicine._id} /></td>
+        <td>{medicine?.salePrice * quantity}</td>
+        <td><button>Add</button></td>
+
+    </tr>
+}
