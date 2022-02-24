0ed4fc6523bd0deacdc10f40235873bd9756f7b9
Nahid51 <hasannahidnahid@gmail.com>
2022-02-25 00:38:28 +0600

    implemented update doctor section

--- src/components/Dashboard/DoctorsDashboard/DoctorProfile/DoctorProfile.js
+++ src/components/Dashboard/DoctorsDashboard/DoctorProfile/DoctorProfile.js
@@ -20,10 +20,37 @@ const DoctorProfile = () => {
             <Container>
                 <div className='row'>
                     <div className='col-12 col-sm-6 col-md-6 col-lg-4'>
-                        <Card className="text-center">
+                        <Card className="text-center card-control2">
                             <Card.Img className="w-50 mx-auto rounded-circle mt-3" variant="top" src={`data:image/*;base64,${singleDoctorInfo?.photo}`} />
                             <Card.Body>
                                 <Card.Title>{singleDoctorInfo?.name}</Card.Title>
+                                <Card.Text>{singleDoctorInfo?.title}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card className="card-control2 mt-5">
+                            <Card.Body>
+                                <Card.Title>Info</Card.Title>
+                                <Card.Text className='mt-3'><i class="fas fa-home"></i> Address: <br />{singleDoctorInfo?.address}</Card.Text>
+                                <Card.Text className='mt-3'><i class="fas fa-envelope"></i> Email: <br />{singleDoctorInfo?.email}</Card.Text>
+                                <Card.Text className='mt-3'><i class="fas fa-mobile"></i> Mobile: <br />{singleDoctorInfo?.phone}</Card.Text>
+                                <Card.Text className='mt-3'><i class="fas fa-birthday-cake"></i> Birth Date: <br />{singleDoctorInfo?.birthday}</Card.Text>
+                                <hr />
+                                <Card.Title className='mb-3'>Social</Card.Title>
+                                <a href={singleDoctorInfo?.twitter}
+                                    className="text-dark text-decoration-none"
+                                >
+                                    <i className="fab fa-twitter me-3"></i>Twitter
+                                </a> <br />
+                                <a href={singleDoctorInfo?.facebook}
+                                    className="text-dark text-decoration-none"
+                                >
+                                    <i class="fab fa-facebook-f me-4"></i>Facebook
+                                </a> <br />
+                                <a href={singleDoctorInfo?.linkedin}
+                                    className="text-dark text-decoration-none"
+                                >
+                                    <i class="fab fa-linkedin-in me-4"></i>Linkedin
+                                </a>
                             </Card.Body>
                         </Card>
                     </div>
