51f30ed81b26f98a9884e2461b13b6d08efc1f0b
ruhulamin77 <ruhulaminbd93@gmail.com>
2023-05-02 00:50:51 +0600

    firebase auth change

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -4,6 +4,6 @@ const firebaseConfig = {
   projectId: 'react-auth-integration-b2f4b',
   storageBucket: 'react-auth-integration-b2f4b.appspot.com',
   messagingSenderId: '635726021775',
-  appId: '1:635726021775:web:ec37124e0d274812cadcfa',
+  appId: '1:635726021775:web:c6498f7ba46807e2cadcfa',
 };
 export default firebaseConfig;
